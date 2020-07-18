24ba0af207f77df2f26183641295dd2ee439a042
Nida Munir <nida.munir@emumba.com>
2020-07-18 21:03:19 +0500

    Add Image input list

---
+++ app/components/ImageInputList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import ImageInput from "./ImageInput";
+
+function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  return (
+    <View style={styles.container}>
+      {imageUris.map((uri) => {
+        return (
+          <View key={uri} style={styles.image}>
+            <ImageInput
+              imageUri={uri}
+              onChangeImage={() => {
+                onRemoveImage(uri);
+              }}
+            />
+          </View>
+        );
+      })}
+      <ImageInput
+        imageUri={null}
+        onChangeImage={(uri) => {
+          onAddImage(uri);
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  image: {
+    marginRight: 10,
+  },
+});
+
+export default ImageInputList;
