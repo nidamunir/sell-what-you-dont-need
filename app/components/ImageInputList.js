4f9b02ee2db59a29963a30b81df1f8a2f1a2cf46
Nida Munir <nida.munir@emumba.com>
2020-07-19 08:37:01 +0500

    Enable Horizontal Scrolling in ImageInputList

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,29 +1,39 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => {
-        return (
-          <View key={uri} style={styles.image}>
-            <ImageInput
-              imageUri={uri}
-              onChangeImage={() => {
-                onRemoveImage(uri);
-              }}
-            />
-          </View>
-        );
-      })}
-      <ImageInput
-        imageUri={null}
-        onChangeImage={(uri) => {
-          onAddImage(uri);
-        }}
-      />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+        horizontal
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => {
+            return (
+              <View key={uri} style={styles.image}>
+                <ImageInput
+                  imageUri={uri}
+                  onChangeImage={() => {
+                    onRemoveImage(uri);
+                  }}
+                />
+              </View>
+            );
+          })}
+          <ImageInput
+            imageUri={null}
+            onChangeImage={(uri) => {
+              onAddImage(uri);
+            }}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
