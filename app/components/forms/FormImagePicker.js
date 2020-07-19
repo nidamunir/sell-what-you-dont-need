af4ef33bf775b2557b278b3d07e0eb3ed756f146
Nida Munir <nida.munir@emumba.com>
2020-07-19 08:47:23 +0500

    Add FormImagePicker Component

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import { ErrorMessage } from "./ErrorMessage";
+import ImageInputList from "../ImageInputList";
+
+function FormImagePicker({ name }) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleImageAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+
+  const handleImageRemove = (uri) => {
+    setFieldValue(
+      name,
+      imageUris.filter((u) => u != uri)
+    );
+  };
+
+  return (
+    <>
+      <ImageInputList
+        imageUris={imageUris}
+        onRemoveImage={handleImageRemove}
+        onAddImage={handleImageAdd}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default FormImagePicker;
